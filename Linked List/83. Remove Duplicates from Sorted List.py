4950593cc2b640dc66872c616b07959addac3de2
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-01-03 04:54:41 +1300

    Add files via upload

---
+++ Linked List/83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+        cur = None
+        while head:
+            if not cur or cur.val != head.val:
+                if not res:
+                    res = ListNode(head.val)
+                    cur = res
+                else:
+                    cur.next = ListNode(head.val)
+                    cur = cur.next
+            head = head.next
+        return res
+
+        '''
+        # simple answer
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
+        '''
