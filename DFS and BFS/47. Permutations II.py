41515151da3244633d33b480f189f33df721a5b2
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-02-02 00:56:30 +1300

    Add files via upload

---
+++ DFS and BFS/47. Permutations II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        r = len(nums)
+
+        res = [0] * r
+        check = [False] * len(nums)
+        answer = []
+
+        def dfs(start):
+            if start == r and not res in answer:
+                answer.append(list(res))
+            else:
+                for i in range(len(nums)):
+                    if not check[i]:
+                        res[start] = nums[i]
+                        check[i] = True
+                        dfs(start + 1)
+                        check[i] = False
+
+        dfs(0)
+        return answer
