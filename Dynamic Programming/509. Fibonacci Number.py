463d6f5895bf57c2c76bd7d9e3a399a939230e0c
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-02-20 21:58:53 +1300

    Update 509. Fibonacci Number.py

--- Dynamic Programming/509. Fibonacci Number.py
+++ Dynamic Programming/509. Fibonacci Number.py
@@ -1,13 +1,7 @@
-import os
-import datetime as dt
-
-if __name__ == "__main__":
-    print("hello")
-    source_directory = input()
-    try:
-        for file in os.listdir(source_directory):
-            file_path = os.path.join(source_directory, file)
-            file_creation_time = dt.datetime.fromtimestamp(os.path.getctime(file_path))
-            print(file + "   " + str(file_creation_time))
-    except OSError as error:
-        print(error)
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0, 1]
+        for i in range(2, n+1):
+            dp.append(dp[i-2]+dp[i-1])
+        return dp[n]
+        
