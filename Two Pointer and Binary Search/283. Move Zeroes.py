abf5d2373240f46402368b064653acaf6b12167a
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-03-15 18:53:21 +1300

    Add files via upload

---
+++ Two Pointer and Binary Search/283. Move Zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i, j = 0, 1
+        while i < len(nums) and j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                i += 1
+                j += 1
+            elif nums[i] == 0 and nums[j] == 0:
+                j += 1
+            else:
+                i += 1
+                j += 1
