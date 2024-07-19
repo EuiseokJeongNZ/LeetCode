8ba77151d0bd7e81e69adb23801bb4b83f7dc339
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-07-19 22:04:00 +0900

    Add files via upload

--- Heap and Tree/700. Search in a Binary Search Tree.py
+++ Heap and Tree/700. Search in a Binary Search Tree.py
@@ -1,21 +1,23 @@
-import sys
-from collections import deque
-
-n = int(sys.stdin.readline())
-#farm = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-visited = [[1]*n for _ in range(n)]
-
-di = [-1, 1, 0, 0]
-dj = [0, 0, -1, 1]
-
-for i in range(n):
-    for j in range(n):
-        if n//2==i or n//2==j or abs(n//2-i)==j or abs(n//2-j)==i or (abs(n//2-i)==abs(n//2-j)):
-            visited[i][j] = 0
-        if (i==0 and j==0) or (i==n-1 and j==n-1):
-            visited[i][j] = 1
-
-print(visited)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        self.ans = None
 
+        def find(root):
+            if not root:
+                return
+
+            find(root.left)
+            find(root.right)
+            if root.val == val:
+                self.ans = root
+                return
+
+        find(root)
+        return self.ans
