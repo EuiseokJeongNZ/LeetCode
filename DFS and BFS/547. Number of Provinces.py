233a9a5b399ba19902ac4283db48635f0105143d
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-09-03 21:49:41 +0900

    Update 547. Number of Provinces.py

--- DFS and BFS/547. Number of Provinces.py
+++ DFS and BFS/547. Number of Provinces.py
@@ -1,57 +1,28 @@
-class dfs_class:
-    def __init__(self):
-        self.graph = [
-            [1, 2],
-            [4, 5],
-            [3],
-            [5],
-            [],
-            []
-        ]
-        self.visited = [False] * 6
-        self.res = []
-    def dfs(self, start : int):
-        if self.visited[start]:
-            return
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(v):
+            visited[v] = True
+            for n in dic[v]:
+                if not visited[n]:
+                    dfs(n)
 
-        self.res.append(start+1)
-        self.visited[start] = True
+        dic = {i+1:[] for i in range(len(isConnected))}
+        visited = [False]*(len(isConnected)+1)
+        res = 0
 
-        for i in self.graph[start]:
-            if not self.visited[i]:
-                self.dfs(i)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+                if i == j:
+                    continue
+                else:
+                    if isConnected[i][j] == 0:
+                        continue
+                    else:
+                        dic[i+1].append(j+1)
+        
+        for i in range(1, len(isConnected)+1):
+            if not visited[i]:
+                res += 1
+                dfs(i)
 
-class my_dfs:
-    def __init__(self):
-        self.graph = {
-            1: [2, 3],
-            2: [1, 5, 6],
-            3: [1, 4],
-            4: [3, 6],
-            5: [2],
-            6: [2, 4]
-        }
-        self.visited = [False]*(6+1)
-        self.res = []
-    def traversal(self, start: int):
-        self.visited[start] = True
-        self.res.append(start)
-
-        for i in self.graph[start]:
-            if not self.visited[i]:
-                self.traversal(i)
-
-
-if __name__ == '__main__':
-
-    # 창중이 코드
-    dfs = dfs_class()
-
-    dfs.dfs(0)
-    print(dfs.res)
-    print()
-
-    # 내코드
-    euiseok_dfs = my_dfs()
-    euiseok_dfs.traversal(1)
-    print(euiseok_dfs.res)
+        return res
