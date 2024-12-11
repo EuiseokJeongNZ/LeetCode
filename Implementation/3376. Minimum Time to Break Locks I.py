ebf872c438a784b0e107994351021075973ebc71
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2024-12-11 14:08:50 +0900

    Add files via upload

---
+++ Implementation/3376. Minimum Time to Break Locks I.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+
+class Solution:
+    def findMinimumTime(self, strength: List[int], K: int) -> int:
+        def cal(s):
+            res = 0
+            x = 1
+            for val in s:
+                req = math.ceil(val / x)
+                res += req
+                x += K
+            return res
+
+        val = float('inf')
+        for s in permutations(strength):
+            val = min(val, cal(s))
+
+        return val
